4fe34e9304413f123c3e7369b716c9f847ae837f
hatimbr <hatim-boutejdir@hotmail.com>
2025-07-31 19:09:12 +0100

    I make a weather _advice as an execise

---
+++ python_introduction/test.py
@@ -0,0 +1,31 @@
+# false structure 
+# for number in range(2, 9 , 2):
+    #print(number)
+
+#print("We have 4 enven numbers")
+
+#count = 0
+#for number in range (1, 10):
+    #if number % 2 == 0:
+        #count += 1
+        #print(number)
+
+#print(f"We have {count} even numbers")
+
+#for i in range(1, 11):
+  # Outer loop iterates through rows (multiplication factors)
+  #for j in range(1, 11):
+    # Inner loop iterates through columns (other factors)
+    #product = i * j
+    #print(f"{i} x {j} = {product}", end="\t")  # Print with tabs for better formatting
+  #print()  # Move to a new line after each row
+
+#rows = 5
+
+#for i in range(1, rows + 1):
+  # Outer loop controls the number of rows
+  #for j in range(1, i + 1):
+    # Inner loop prints asterisks for each row
+    #print("*", end="")
+  #print()  # Move to a new line after each row of asterisks
+
